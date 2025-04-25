002d572d6f8efb5c5faf86ca2a99cc23230f8752
tyharpercooks <tyharper@unc.edu>
2025-04-25 18:37:50 -0400

    update

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, request, jsonify, render_template
 
+## This imports the configuration parser library
+import configparser
+
+## This creates an instance of its 'ConfigParser' tool
+cfg = configparser.ConfigParser()
+## This reads data from 'config.ini' into that 'cfg' variable
+cfg.read(".env")
+import mysql.connector
+
 app = Flask(__name__)
+
+
+def runQuery(sql):
+    dbconn = mysql.connector.connect(
+        user=cfg["db"]["user"],
+        password=cfg["db"]["password"],
+        host=cfg["db"]["host"],
+        database=cfg["db"]["database"],
+    )
+    cursor = dbconn.cursor()
+    cursor.execute(sql)
+    dbconn.commit()
+    dbconn.close()
 
 
 # render templates
@@ -30,13 +52,29 @@ def echo():
     return jsonify(data)
 
 
-# POST route with form data
-@app.route("/submit-form", methods=["POST"])
-def submit_form():
-    name = request.form.get("name")
-    email = request.form.get("email")
-    return f"Received form submission: {name}, {email}"
+# # POST route with form data
+# @app.route("/submit-form", methods=["POST"])
+# def submit_form():
+#     data = {key: request.form.getlist(key) for key in request.form.keys()}
+#     return jsonify(data)
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5050)
+
+
+# POST route with form data
+@app.route("/submit-form", methods=["POST"])
+def submit_form():
+    data = {key: request.form.getlist(key) for key in request.form.keys()}
+    # return jsonify(data)
+    # sql = "INSERT INTO honorsurvey_responses (gender, a1c, heardaboutstudy) VALUES ('150', 'male', 'event|email');"
+    sql = "INSERT INTO honorsurvey_responses (gender, a1c, heardaboutstudy) VALUES ('"
+    sql += data["a1c"][0]
+    sql += "', '"
+    sql += data["gender"][0]
+    sql += "', '"
+    sql += "|".join(data["heardaboutstudy"])
+    sql += "');"
+    runQuery(sql)
+    return sql
